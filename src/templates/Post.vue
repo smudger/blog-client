6c6653c6027be9dfc86044ef0a6d03d12664b172
smudger <15144861+smudger@users.noreply.github.com>
2020-05-23 19:49:59 +0100

    Add info box

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="max-w-lg mx-auto text-xl">
-      <div class="flex justify-between">
-        <h1 id="title" class="underline">
+    <div class="mx-auto text-xl">
+      <div id="info-box" class="flex flex-col border rounded-lg bg-gray-900 text-gray-100 p-6 mb-6">
+        <h1 class="border-b-2 text-3xl mr-3 mb-3" id="title">
           {{ $page.post.title.toLowerCase() }}
         </h1>
+        <p id="time_to_read">{{ $page.post.timeToRead }} mins to read</p>
         <p id="author">{{ $page.post.author.toLowerCase() }}</p>
+        <p id="created_at">{{ $page.post.created_at | humanise }}</p>
+        <div class="flex flex-wrap">
+          <div
+            class="uppercase tracking-wider font-bold text-sm mr-2 mt-4 bg-gray-100 text-gray-900 border px-2 py-1 rounded-lg"
+            v-for="tag in $page.post.tags"
+            :key="tag"
+          >
+            {{ tag }}
+          </div>
+        </div>
       </div>
       <div
         id="content"
@@ -14,12 +25,27 @@
     </div>
 </template>
 
+<script>
+import moment from 'moment'
+
+export default {
+  filters: {
+    humanise (date) {
+      return moment(date).fromNow()
+    }
+  }
+}
+</script>
+
 <page-query>
   query($id: ID!) {
     post(id: $id) {
       author
+      created_at
       title
       content
+      tags
+      timeToRead
     }
   }
 </page-query>
